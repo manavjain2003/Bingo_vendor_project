e9cac55457214b282ccff0680d0e1441e027f830
Manav Jain <manav7421@gmail.com>
2026-08-09 23:34:57 +0530

    swagger api for presentation

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 const categoryRoutes = require('./routes/categoryRoutes');
@@ -14,9 +17,12 @@ const { notFoundHandler, errorHandler } = require('./middleware/errorHandler');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const openapiSpec = YAML.load(path.join(__dirname, '..', 'openapi.yaml'));
 
 app.use(cors({ origin: process.env.CORS_ORIGIN || '*' }));
 app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpec));
 
 app.use('/vendors', vendorRoutes);
 app.use('/auth', authRoutes);
